a123305ef134d53a70ea23c991ca10789a16b098
Thesko27 <franthesko1984@gmail.com>
2023-06-14 17:01:25 -0300

    upDate

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+let sec = document.querySelectorAll("section");
+let links = document.querySelectorAll("nav a");
+
+//Funcão
+window.onscroll = () => {
+    sec.forEach((section) => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 60;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            links.forEach((link) => {
+                link.classList.remove("active");
+                document
+                    .querySelector(`nav a[href*=` + id + `]`)
+                    .classList.add("active");
+            });
+        }
+    });
+};
